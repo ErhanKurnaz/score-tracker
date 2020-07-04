325383322fbb217ecafb591a97bc62ccfee6f543
Erhan Kurnaz <erhan@globalscreen.nl>
2020-07-04 23:09:22 +0200

    added user interface

--- api/src/services/users/users.service.ts
+++ api/src/services/users/users.service.ts
@@ -4,6 +4,7 @@ import { Application } from '../../declarations'
 import { Users } from './users.class'
 import createModel from '../../models/users.model'
 import hooks from './users.hooks'
+import { IUser } from '../../models/users.interface'
 
 // Add this service to the service type index
 declare module '../../declarations' {
@@ -23,9 +24,6 @@ export default function (app: Application) {
 
     // Get our initialized service so that we can register hooks
     const service = app.service('users')
-    service.on('created', user => {
-        console.log(user)
-    })
 
     service.hooks(hooks)
 }
